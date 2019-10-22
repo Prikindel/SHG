ebecc5dba749a27fd917c83f73e288a3976d18c4
Prikindel <kiryakovm96@gmail.com>
2019-10-22 09:50:13 +0300

    Add pins ESP32 for Shield

---
+++ greenhouse/server/pinoutForShield.h
@@ -0,0 +1,48 @@
+/*
+ *  Pins shield -> esp32:
+ *  <--- char D and PIN ---->
+ *  <------- DIGITAL ------->
+ *  D0    ->    0
+ *  D1    ->    1
+ *  D2    ->    IO26
+ *  D3    ->    IO25
+ *  D4    ->    IO17
+ *  D5    ->    IO16
+ *  D6    ->    IO27
+ *  D7    ->    IO14
+ *  D8    ->    IO12
+ *  D9    ->    IO13
+ *  <-------- SPI --------->
+ *  D10   ->    IO5
+ *  D11   ->    IO23
+ *  D12   ->    IO19
+ *  D13   ->    IO18
+ *  <------- ANALOG ------->
+ *  A0    ->    IO2
+ *  A1    ->    IO4
+ *  A2    ->    IO36
+ *  A3    ->    IO34
+ *  A4    ->    IO38
+ *  A5    ->    IO39
+ */
+
+#define   D0    0
+#define   D1    1
+#define   D2    26
+#define   D3    25
+#define   D4    17
+#define   D5    16
+#define   D6    27
+#define   D7    14
+#define   D8    12
+#define   D9    13
+#define   D10   5
+#define   D11   23
+#define   D12   19
+#define   D13   18
+#define   A0    2
+#define   A1    4
+#define   A2    36
+#define   A3    34
+#define   A4    38
+#define   A5    39
