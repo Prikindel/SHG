8f78561dadcba4c484dbdad98bff36cf0c4625e1
Ivan <vin.ven@yandex.ru>
2019-10-28 13:17:04 +0300

    28.10.19

--- greenhouse/server/server.ino
+++ greenhouse/server/server.ino
@@ -13,7 +13,13 @@ const char* password = "uuubbb555";                            //Password WiFi
 
 const int countJSON = 5;                              //size JSON
 String outJSON[countJSON];                            //value JSON
-String outIndexJSON[countJSON];                       //key JSON
+String outIndexJSON[countJSON] = {                    //key JSON
+                                  "temperature",  //-температура
+                                  "humidity",     //-влажность 
+                                  "pressure",     //-давление
+                                  "co2",          //-CO2
+                                  "water"         //-влажность почвы
+                                  };
 
 IPAddress ip(192, 168, 43, 155);                      //IP server
 IPAddress gateway(192, 168, 1, 1);                    //sluice
@@ -61,25 +67,160 @@ void handleRoot()
   Serial.println();
   Serial.println("Open main page");
   server.send(200, "text/html", MAIN_page);
-  
 }
 
 //
-//  FAN
+//  Temperature data transmission
 //
 
-void fan()
-{
+void temperatureTransmission() {
+  // Get temperature
+  getParameter(0, TEMPERATURA);
+}
+
+//
+//  Humidity data transmission
+//
+
+void humidityTransmission() {
+  // Get humidity
+  getParameter(1, HUMIDITY);
+}
+
+//
+//  Pressure data transmission
+//
+
+void pressureTransmission() {
+  // Get pressure
+  getParameter(2, PRESSURE);
+}
+
+//
+//  CO2
+//
+
+void co2Transmission() {
+  // Get CO2
+  getParameter(3, CO2);
+}
+
+//
+//  WATER
+//
+
+void waterTransmission() {
+  // Get lvl water
+  getParameter(4, WATER);
+}
+
+//
+//  Receiving data depending on the code
+//
+
+void getParameter(int index, int code) {
+    outJSON[index] = getDataCode(code);
+}
+
+//
+//  OUT JSON
+//
+
+void outData() {
+  Serial.println();
+  Serial.println("Open getdata page");
+  temperatureTransmission();
+  humidityTransmission();
+  pressureTransmission();
+  co2Transmission();
+  waterTransmission();
+  
+  String out = "{";
+  for ( int i = 0; i < countJSON; i++ ) {
+    
+    out += "\"" + outIndexJSON[i] + "\":" /*+ "\""*/ + outJSON[i] /*+ "\""*/;
+    if (i < countJSON - 1) {
+      out += ",";
+    }
+  }
+  out += "}";
+  Serial.println(out);
+  server.send(200, "text/html", out);
+}
+
+//
+//  fan ON/OFF
+//
+
+void fanOnOff() {
+  Serial.println();
+  Serial.println("Open open-door page");
+  if(server.args() != 0) {
+    fan = server.arg("fan").toInt();
+  }
+  // Управляем вентилятором здесь
+  Serial.print(fan == 0 ? "OFF" : "ON");
+  Serial.println(" fan");
+  String feedback = getDataCode(FAN);
+  String out = "";
+  Serial.println(feedback);
+  if (feedback == "false" || feedback == "") {
+    out = "ERROR";
+  } else {
+    out = "fan = " + (String)fan;
+  }
+  server.send(200, "text/html", out);
+}
+
+//
+// OPEN DOOR
+//
+
+void openDoor() {
+  Serial.println();
+  Serial.println("Open open-door page");
+  if(server.args() != 0) {
+    doorInt = server.arg("door").toInt();
+  }
+  // Управляем дверью здесь
+  Serial.print(doorInt == 0 ? "CLOSE" : ("OPEN on " + (String)doorInt));
+  Serial.println(" door");
+  String feedback = getDataCode(DOOR);
+  String out = "";
+  Serial.println(feedback);
+  if (feedback == "false" || feedback == "") {
+    out = "ERROR";
+  } else {
+    out = "door = " + (String)doorInt;
+  }
+  server.send(200, "text/html", out);
 }
 
 //
 //  LIGHT
 //
-int l = 0;
-void light()
-{
-  l = (l == 0)?1:0;
-  digitalWrite(5, l);
+
+void lightRoom() {
+  Serial.println();
+  Serial.println("Open light-room page");
+  if(server.args() != 0) {
+    red   = server.arg("red").toInt();
+    green = server.arg("green").toInt();
+    blue  = server.arg("blue").toInt();
+    light = server.arg("light").toInt();
+  }
+  // Управляем светом здесь
+  Serial.print(light == 0 ? "OFF" : "ON");
+  Serial.println(" light");
+  String feedback = getDataCode(LEDRING);
+  String out = "";
+  Serial.println(feedback);
+  if (feedback == "false" || feedback == "") {
+    out = "ERROR";
+  } else {
+    out = "light = " + (String)light + " RGB(" + red + ";" + green + ";" + blue + ")";
+  }
+  server.send(200, "text/html", out);
 }
 
 //
